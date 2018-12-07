733ba0f5b422881b0bd9e64db955748dfc1e320d
Arismendys Marte <arismendys@gmail.com>
2018-12-07 14:15:47 -0400

    Update ListaEstudiantes.java

--- ListaEstudiantes.java
+++ ListaEstudiantes.java
@@ -1,18 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Listas;
 
 import java.io.*;
-import java.util.Scanner;
 
-
-/**
- *
- * @author Arismendys Marte
- */
 public class ListaEstudiantes {
         public static void main(String[] args) throws FileNotFoundException, IOException  
     {
